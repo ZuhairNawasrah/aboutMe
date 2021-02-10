d9e661a52d4c15400e3ccfb5ec00c6bbd9f7851e
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-02-10 07:10:36 +0200

    edit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Guessing game about me.
 
+I build a game that let the user guess some basic data about me. The purpose of it just for fun, where the first part of the game was with (YES/NO) questions, and that is the easiest part of the game, and the second part is a little bit harder, which was guess the answer without giving a choices to the user, but it has more than one attempt, as well as a hint that's appear as alert after each attempt.
+
+Author: **Zuhair.N**
+
+**Links**:
+[Guessing game](https://zuhairnawasrah.github.io/aboutMe/)
+[Code of the game](https://github.com/ZuhairNawasrah/aboutMe)
--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 //------------------------------------Lab-02/Q-1-----------------------------------------//
 let userName = prompt('What\'s your name?');
 let amIPro = prompt('Am I a professional Web Developer(Y/N)?');
-let mark=0;
+let mark = 0;
 if (amIPro.toUpperCase() === 'Y') {
   alert('Thank you for your response, which is Y');
-  mark+=1;
+  mark += 1;
   // console.log('Your answer is correct ♥');
 } else {
   alert('Nope, I am pro, but you answer with is Y');
@@ -14,7 +14,7 @@ if (amIPro.toUpperCase() === 'Y') {
 let engLevel = prompt('Am I a native english speaker(Y/N)?');
 if (engLevel.toUpperCase() === 'N') {
   alert('You already know about me by answering (' + engLevel.toUpperCase() + ')');
-  mark+=1;
+  mark += 1;
   // console.log('Your answer is correct ♥');
 } else {
   alert('Actually I am not, you really don\'t know many things about me');
@@ -23,7 +23,7 @@ if (engLevel.toUpperCase() === 'N') {
 let likeWork = prompt('Do I like my work(Y/N)?');
 if (likeWork.toUpperCase() === 'Y') {
   alert('As you said, of course I do ');
-  mark+=1;
+  mark += 1;
   // console.log('Your answer is correct ♥');
 } else {
   alert('Your answer is wrong, I really like it');
@@ -32,7 +32,7 @@ if (likeWork.toUpperCase() === 'Y') {
 let favColor = prompt('Do I like White as my favorite color(Y/N)?');
 if (favColor.toUpperCase() === 'Y') {
   alert('Yp...for sure');
-  mark+=1;
+  mark += 1;
   // console.log('Your answer is correct ♥');
 }
 else {
@@ -42,7 +42,7 @@ else {
 let favMusic = prompt('Do I like Jazz as my favorite music(Y/N)?');
 if (favMusic.toUpperCase() === 'N') {
   alert('That\'s right...I hate that type a lot');
-  mark+=1;
+  mark += 1;
   // console.log('Your answer is correct ♥');
 } else {
   alert('You have to search about me, so you learn a bit about me, because I don\'t like it ');
@@ -60,7 +60,7 @@ for (let i = 0; i < attempt; i = 0) {
     alert('It\'s too close, you now have ' + attempt + ' tries left.');
   } else if (guessNo === '9') {
     attempt -= 1;
-    mark+=1;
+    mark += 1;
     alert('Congratulations, your guess is correct!!♥♥♥');
     if (attempt > 0) {
       alert('You still have ' + attempt + ' tries');
@@ -76,27 +76,28 @@ if (guessNo === '9') {
 //---------------------------------------Q-7---------------------------------------------//
 let favoriteColor = '';
 let colorAttempt = 6;
-let condition=true;
+let condition = true;
 let colorArr = ['purple', 'white', 'black', 'light gary', 'green'];
 for (let i = 0; i < colorAttempt && condition; i = 0) {
   favoriteColor = prompt('Can you guess one of my favorite colors?').toLowerCase();
   switch (favoriteColor) {
-  case colorArr[0]:
-  case colorArr[1]:
-  case colorArr[2]:
-  case colorArr[3]:
-  case colorArr[4]:
-    colorAttempt-=1;
-    mark+=1;
-    alert('Congratulations, your guess is correct!!♥♥♥');
-    condition=false;
-    if (colorAttempt > 0) {
-      alert('You still have ' + colorAttempt + ' tries');}
-    break;
-  default:
-    colorAttempt-=1;
-    alert('Nope, you didn\'t guess a correct answer, now you have ' + colorAttempt + ' tries left.');
-    break;
+    case colorArr[0]:
+    case colorArr[1]:
+    case colorArr[2]:
+    case colorArr[3]:
+    case colorArr[4]:
+      colorAttempt -= 1;
+      mark += 1;
+      alert('Congratulations, your guess is correct!!♥♥♥');
+      condition = false;
+      if (colorAttempt > 0) {
+        alert('You still have ' + colorAttempt + ' tries');
+      }
+      break;
+    default:
+      colorAttempt -= 1;
+      alert('Nope, you didn\'t guess a correct answer, now you have ' + colorAttempt + ' tries left.');
+      break;
   }
 }
 document.write(`My favorite colors are: ${colorArr}, and your mark for whole game is: ${mark}`);
