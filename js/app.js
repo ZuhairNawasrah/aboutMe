2b26d885f4f35cd60a373d2204f58bbec41f887b
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-02-11 00:36:40 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,34 +1,35 @@
 
 //------------------------------------Lab-02/Q-1-----------------------------------------//
 let userName = prompt('What\'s your name?');
-let amIPro = prompt('Am I a professional Web Developer(Y/N)?');
 let mark = 0;
-questOne();
+alert('Welcome to our website Mr.' + userName);
+document.getElementById('welcome').innerHTML = 'Welcome ' + userName + ', it\'s a glad to see you joining our website.';
 function questOne() {
+  let amIPro = prompt('Am I a professional Web Developer(Y/N)?');
   if (amIPro.toUpperCase() === 'Y') {
     alert('Thank you for your response, which is Y');
     mark += 1;
   // console.log('Your answer is correct ♥');
   } else {
-    alert('Nope, I am pro, but you answer with is Y');
+    alert('Nope, I am pro, but you don\'t know me well!');
   }
 }
+questOne();
 //---------------------------------------Q-2---------------------------------------------//
-let engLevel = prompt('Am I a native english speaker(Y/N)?');
-questTwo();
 function questTwo() {
+  let engLevel = prompt('Am I a native english speaker(Y/N)?');
   if (engLevel.toUpperCase() === 'N') {
     alert('You already know about me by answering (' + engLevel.toUpperCase() + ')');
     mark += 1;
-  // console.log('Your answer is correct ♥');
+    // console.log('Your answer is correct ♥');
   } else {
     alert('Actually I am not, you really don\'t know many things about me');
   }
 }
+questTwo();
 //---------------------------------------Q-3---------------------------------------------//
-let likeWork = prompt('Do I like my work(Y/N)?');
-questThree();
 function questThree() {
+  let likeWork = prompt('Do I like my work(Y/N)?');
   if (likeWork.toUpperCase() === 'Y') {
     alert('As you said, of course I do ');
     mark += 1;
@@ -37,36 +38,36 @@ function questThree() {
     alert('Your answer is wrong, I really like it');
   }
 }
+questThree();
 //---------------------------------------Q-4---------------------------------------------//
-let favColor = prompt('Do I like White as my favorite color(Y/N)?');
-questFour();
 function questFour() {
+  let favColor = prompt('Do I like White as my favorite color(Y/N)?');
   if (favColor.toUpperCase() === 'Y') {
     alert('Yp...for sure');
     mark += 1;
-  // console.log('Your answer is correct ♥');
+    // console.log('Your answer is correct ♥');
   }
   else {
     alert('Nope...I really like it');
   }
 }
+questFour();
 //---------------------------------------Q-5---------------------------------------------//
-let favMusic = prompt('Do I like Jazz as my favorite music(Y/N)?');
-questFive();
 function questFive() {
+  let favMusic = prompt('Do I like Jazz as my favorite music(Y/N)?');
   if (favMusic.toUpperCase() === 'N') {
     alert('That\'s right...I hate that type a lot');
     mark += 1;
-  // console.log('Your answer is correct ♥');
+    // console.log('Your answer is correct ♥');
   } else {
     alert('You have to search about me, so you learn a bit about me, because I don\'t like it ');
   }
 }
+questFive();
 //------------------------------------Lab-03/Q-6-----------------------------------------//
-let attempt = 4;
-let guessNo = '';
-questSix();
 function questSix() {
+  let attempt = 4;
+  let guessNo = '';
   for (let i = 0; i < attempt; i = 0) {
     guessNo = prompt('Can you guess what\'s my favorite number (1~10)?');
     if (guessNo <= '5') {
@@ -91,13 +92,13 @@ function questSix() {
     alert('Unfortunately, you didn\'t guess the right answer, which is (9)');
   }
 }
+questSix();
 //---------------------------------------Q-7---------------------------------------------//
-let favoriteColor = '';
-let colorAttempt = 6;
-let condition = true;
-let colorArr = ['purple', 'white', 'black', 'light gary', 'green'];
-questSeven();
+const colorArr = ['purple', 'white', 'black', 'light gary', 'green'];
 function questSeven() {
+  let favoriteColor = '';
+  let colorAttempt = 6;
+  let condition = true;
   for (let i = 0; i < colorAttempt && condition; i = 0) {
     favoriteColor = prompt('Can you guess one of my favorite colors?').toLowerCase();
     switch (favoriteColor) {
@@ -121,8 +122,13 @@ function questSeven() {
     }
   }
 }
-document.write(`My favorite colors are: ${colorArr}, and your mark for whole game is: ${mark}`);
+questSeven();
+let colorList='<ul>';
+for (let i = 0; i < colorArr.length ; i++) {
+  colorList=colorList+'<li>'+colorArr[i]+'</li>';
+}
+colorList=colorList+'</ul>';
+alert(`Your score is (${mark}/7)`);
+document.getElementById('colorList').innerHTML = `My favorite colors list as follow: ${colorList}`;
 
 
-document.getElementById('h1').innerHTML = 'Welcome ' + userName + ', it\'s a glad to see you joining our website.';
-alert('Welcome to our website Mr.' + userName);
